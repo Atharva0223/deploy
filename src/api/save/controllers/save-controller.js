bfef4a14b2ae13651eda8a240d47e37b757516ef
Atharva0223 <atharva@geexu.in>
2023-04-21 11:50:28 +0530

    made some changes in shares and saves api's

--- src/api/save/controllers/save-controller.js
+++ src/api/save/controllers/save-controller.js
@@ -10,7 +10,7 @@ module.exports = {
       DELETE FROM saves s
       USING saves_opportunity_links sol, saves_user_links sul
       WHERE s.id = sol.save_id AND sul.save_id = s.id AND
-      s.is_deleted = false AND s.id = $1;
+      s.is_deleted = false AND sol.id = $1;
     `;
 
       const data = await client.query(query,[ctx.params.id]);
@@ -26,15 +26,43 @@ module.exports = {
       const client = await connect();
       const query = `
       SELECT
-      COALESCE(sul.user_id) AS "User",
-      COALESCE(s.save) AS "Saved",
-      COALESCE(sol.opportunity_id) AS Opportunity
+      org_logo.url AS "Organization logo",
+      COALESCE(org.name, '') AS "Organization name",
+      COALESCE(opp.profile, '') AS "Opportunity profile",
+      COALESCE(opp.city, '') AS "City",
+      COALESCE(opp_image.url, '') AS "Opportunity image",
+      COALESCE(opp.months) AS "Duration",
+      COALESCE(opp.start_on) AS "Start Date",
+      COALESCE(opp.end_on) AS "End Date",
+      COALESCE(ROUND(AVG(r.value), 1), 0) AS "Rating"
       FROM 
       saves s
       LEFT JOIN saves_opportunity_links sol ON s.id = sol.save_id
       LEFT JOIN saves_user_links sul ON sul.save_id = s.id
+      LEFT JOIN opportunities opp ON sol.opportunity_id = opp.id
+      LEFT JOIN opportunities_organization_links ool ON opp.id = ool.opportunity_id
+      LEFT JOIN organizations org ON ool.organization_id = org.id
+      LEFT JOIN opportunities_organization_user_links ooul ON opp.id = ooul.opportunity_id
+      LEFT JOIN ratings_opportunity_links rol ON rol.opportunity_id = opp.id
+      LEFT JOIN ratings r ON r.id = rol.rating_id
+      LEFT JOIN files_related_morphs frm_logo ON frm_logo.related_id = ool.organization_id AND frm_logo.field = 'logo'
+      LEFT JOIN files org_logo ON frm_logo.file_id = org_logo.id
+      LEFT JOIN files_related_morphs frm_image ON frm_image.related_id = opp.id AND frm_image.field = 'image'
+      LEFT JOIN files opp_image ON frm_image.file_id = opp_image.id
       WHERE
       s.is_deleted = false AND sul.user_id = $1
+      GROUP BY
+      sul.user_id,
+      s.save,
+      sol.opportunity_id,
+      org_logo.url,
+      org.name,
+      opp.profile,
+      opp.city,
+      opp_image.url,
+      opp.months,
+      opp.start_on,
+      opp.end_on
     `;
 
       const data = await client.query(query, [ctx.params.id]);
