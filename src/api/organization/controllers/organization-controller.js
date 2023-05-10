06af164a7f1aee3fd02980da749d60b0791f45a1
Atharva0223 <atharva@geexu.in>
2023-05-10 13:04:43 +0530

    view organization by id is completed

--- src/api/organization/controllers/organization-controller.js
+++ src/api/organization/controllers/organization-controller.js
@@ -18,64 +18,47 @@ module.exports = {
           },
         });
 
+      //check if the organization exists or not
+      if (!view) {
+        ctx.send({
+          message: "Organization not found",
+          code: 2,
+        });
+      }
+      if (view) {
+        //get an array of opportunities
+        const opp = [];
+        for (let i = 0; i < view.opportunities.length; i++) {
+          const image = view.opportunities[i].image?.url || null;
+          const oppo = {
+            id: view.opportunities[i].id,
+            profile: view.opportunities[i].profile,
+            image: image,
+          };
+          opp.push(oppo);
+        }
 
-        const [opportunities, organizations_users] = await Promise.all([
-          view.opportunities?.map(async (opportunity) => ({
-            id: opportunity.id,
-            profile: opportunity.profile,
-            image: opportunity.image.url,
-          })),
-          view.organizations_users?.map(async (users) => ({
-            id: users.id,
-            first_name: users.first_name,
-            last_name: users.last_name,
-            // profile_photo: users.profile_photo.url,
-          })),
-        ]);
-        
+        //get an array of organization_users
+        const org_users = [];
+        for (let i = 0; i < view.organization_users.length; i++) {
+          const user = {
+            id: view.organization_users[i].id || null,
+            first_name: view.organization_users[i].first_name || null,
+            last_name: view.organization_users[i].last_name || null,
+          };
+          org_users.push(user);
+        }
 
-        console.log("opportunities", opportunities.Promise,"organizations_users",organizations_users);
-
-      // // initialize empty opportunities array
-      // const opportunities = [];
-
-      // view.opportunities.forEach((opportunity) => {
-      //   const opportunityObj = {
-      //     id: opportunity.id,
-      //     profile: opportunity.profile,
-      //     image: opportunity.image.url,
-      //   };
-
-      //   // push opportunity object into opportunities array
-      //   opportunities.push(opportunityObj);
-      // });
-
-      // // initialize empty organzation users array
-      // const organizations_users = [];
-
-      // view.organizations_users.forEach((users) => {
-      //   const usersObj = {
-      //     id: users.id,
-      //     first_name: users.first_name,
-      //     last_name: users.last_name,
-      //     // profile_photo: users.profile_photo.url,
-      //   };
-
-      //   // push organizations_users object into organizations_users array
-      //   organizations_users.push(usersObj);
-      // });
-
-      // console.log(organizations_users);
-
-      ctx.send({
-        id: view.id,
-        name: view.name,
-        description: view.description,
-        website: view.website,
-        logo: view.logo.url,
-        // opportunities: opportunities,
-        // organizations_users: organizations_users,
-      });
+        ctx.send({
+          id: view.id,
+          name: view.name,
+          description: view.description,
+          website: view.website,
+          logo: view.logo.url,
+          opportunities: opp,
+          organizations_users: org_users,
+        });
+      }
     } catch (error) {
       console.log(error);
     }
